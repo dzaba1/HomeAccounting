e454f22da6a3d66822a0e15d5b99db5fff111f75
Dzaba <blazej.zabialowicz@gmail.com>
2018-07-08 18:15:42 +0200

    Added sqlite provider

---
+++ Src/Dzaba.HomeAccounting.DataBase.EntityFramework.Sqlite/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Dzaba.HomeAccounting.Utils;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dzaba.HomeAccounting.DataBase.EntityFramework.Sqlite
+{
+    public static class Bootstrapper
+    {
+        public static void RegisterSqlite(this IServiceCollection container)
+        {
+            Require.NotNull(container, nameof(container));
+
+            container.AddTransient<IEntityFrameworkProvider, SqliteProvider>();
+        }
+    }
+}
---
+++ Src/Dzaba.HomeAccounting.DataBase.EntityFramework.Sqlite/SqliteProvider.cs
@@ -0,0 +1,23 @@
+﻿using Dzaba.HomeAccounting.Utils;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dzaba.HomeAccounting.DataBase.EntityFramework.Sqlite
+{
+    internal sealed class SqliteProvider : IEntityFrameworkProvider
+    {
+         public void Register(string connectionString, DbContextOptionsBuilder optionsBuilder)
+        {
+            Require.NotWhiteSpace(connectionString, nameof(connectionString));
+            Require.NotNull(optionsBuilder, nameof(optionsBuilder));
+
+            optionsBuilder.UseSqlite(connectionString);
+        }
+
+        public static string CreateConnectionString(string location)
+        {
+            Require.NotEmpty(location, nameof(location));
+
+            return $"Data Source={location}";
+        }
+    }
+}
