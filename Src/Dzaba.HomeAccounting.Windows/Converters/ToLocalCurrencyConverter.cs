3ead7b3c7abd8d110fbac542168b4bfec4c9077f
Dzaba <blazej.zabialowicz@gmail.com>
2019-02-09 12:16:25 +0100

    Fixed currency

---
+++ Src/Dzaba.HomeAccounting.Windows/Converters/ToLocalCurrencyConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Dzaba.HomeAccounting.Windows.Converters
+{
+    public class ToLocalCurrencyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{value:c}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
