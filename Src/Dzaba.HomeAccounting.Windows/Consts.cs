2d8a15745a28aa1843296411170f0a5bde798e26
Dzaba <blazej.zabialowicz@gmail.com>
2019-02-09 14:49:08 +0100

    Fixed date format

--- Src/Dzaba.HomeAccounting.Windows/Consts.cs
+++ Src/Dzaba.HomeAccounting.Windows/Consts.cs
@@ -5,6 +5,6 @@ namespace Dzaba.HomeAccounting.Windows
     public static class Consts
     {
         public static readonly MemberNamePair DefaultMemberPair =
-            new MemberNamePair(null, "Zaden");
+            new MemberNamePair(null, "Żaden");
     }
 }
---
+++ Src/Dzaba.HomeAccounting.Windows/Converters/StringFormatConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Dzaba.HomeAccounting.Windows.Converters
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var formatParam = parameter as string;
+            var format = new StringBuilder("{0");
+            if (!string.IsNullOrWhiteSpace(formatParam))
+            {
+                format.Append(":" + formatParam +  "}");
+            }
+            else
+            {
+                format.Append("}");
+            }
+
+            return string.Format(format.ToString(), value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
